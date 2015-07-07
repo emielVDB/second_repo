c587a7d3dd32f04c7e42297b4c994cc87fda839b
Emiel Vandenbussche <emiel.vandenbussche@zapfi.com>
2015-07-07 14:11:26 +0200

    inittial commit

---
+++ src/Printer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emiel
+ * Date: 7/07/2015
+ * Time: 14:05
+ */
+namespace emiel\printer;
+
+class Printer{
+
+    function __construct()
+    {
+    }
+
+    function printIt(){
+        echo "<br> jupp zeker dadde";
+    }
+}
